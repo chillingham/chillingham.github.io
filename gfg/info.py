469c2e1a0d5e4a35bf380aa9df7685f3e9da6707
chillingham <96072733+chillingham@users.noreply.github.com>
2021-12-19 14:58:22 +0000

    Update info.py

--- gfg/info.py
+++ gfg/info.py
@@ -1,5 +1,8 @@
+from decouple import Config
+
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aagroupspw@gmail.com'
-EMAIL_HOST_PASSWORD = '0n3H3ll0f@W3bs1t3#'
+EMAIL_HOST_PASSWORD = Config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
